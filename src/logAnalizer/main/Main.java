640bd3cfc72e1f481405e404ee30028872ba71fd
fbarocci <federico.barocci@gmail.com>
2015-06-09 17:15:15 +0200

    removed comments

--- src/logAnalizer/main/Main.java
+++ src/logAnalizer/main/Main.java
@@ -21,8 +21,7 @@ public class Main {
 
 		String csvFileName = "data/filter_eventlog.csv";
 
-		TokenReaderFactory tokenReaderFactory = injector
-				.getInstance(TokenReaderFactory.class);
+		TokenReaderFactory tokenReaderFactory = injector.getInstance(TokenReaderFactory.class);
 		TokenReader tokenReader = tokenReaderFactory.create();
 
 		DTWFactory dtwFactory = injector.getInstance(DTWFactory.class);
@@ -41,13 +40,7 @@ public class Main {
 		int sizeAll = timeSeries.size();
 		int sizeSegment = 24;
 
-		/*
-		 * System.out.println(" -> Global tokens count: " + sizeAll);
-		 * timeSeries.unify().forEach((key,value)->{ System.out.println("\t" +
-		 * key + ": " + value); });
-		 */
-
-		System.out.println("Performing DTW test on random subsequances...");
+		System.out.println("Performing DTW test on random subsequences...");
 		System.out.println("Subseries size: " + sizeSegment);
 
 		for (int i = 0;; i++) {
@@ -61,45 +54,15 @@ public class Main {
 
 			System.out.println("" + i + ") TestIndex = " + a + " DateTest = " + timeSeries.getKey(a));
 			
-			System.out.println("" + i
-					+ ") EUCLIDEAN DISTANCE: SolutionIndex = " + e.getIndex()
-					+ " Distance = " + e.getDistance() + " DateSolution = "
-					+ timeSeries.getKey(e.getIndex()));
+			System.out.println("" + i + ") EUCLIDEAN DISTANCE: SolutionIndex = " + e.getIndex()
+					+ " Distance = " + e.getDistance() 
+					+ " DateSolution = " + timeSeries.getKey(e.getIndex()));
 			
-			System.out.println("" + i
-					+ ") MANATTHAN DISTANCE: SolutionIndex = " + m.getIndex()
-					+ " Distance = " + m.getDistance() + " DateSolution = "
-					+ timeSeries.getKey(m.getIndex()));
+			System.out.println("" + i + ") MANATTHAN DISTANCE: SolutionIndex = " + m.getIndex()
+					+ " Distance = " + m.getDistance() 
+					+ " DateSolution = " + timeSeries.getKey(m.getIndex()));
 			
 			System.out.println();
-
-			/*
-			 * System.out.println(" -> t1 tokens count: " + t1.size());
-			 * t1.unify().forEach((key,value)->{ System.out.println("\t" + key +
-			 * ": " + value); });
-			 */
-			/*
-			 * double minDist = Double.POSITIVE_INFINITY; int b = 0;
-			 * 
-			 * for(int j = 0; j < sizeAll-sizeSegment - 1; j++) { if ((j < a -
-			 * sizeSegment) || (j > a + sizeSegment)) { TimeSeries t2 =
-			 * timeSeries.getSegment(j, j + sizeSegment); double dist =
-			 * dtw.DTWDistance(Distances.EUCLIDEAN, t1, t2);
-			 * 
-			 * if (dist < minDist) { minDist = dist; b = j; } } }
-			 */
-
-			/*
-			 * minDist = Double.POSITIVE_INFINITY; b = 0;
-			 * 
-			 * for(int j = 0; j < sizeAll-sizeSegment - 1; j++) { if ((j < a -
-			 * sizeSegment) || (j > a + sizeSegment)) { TimeSeries t2 =
-			 * timeSeries.getSegment(j, j + sizeSegment); double dist =
-			 * dtw.DTWDistance(Distances.MANATTHAN, t1, t2);
-			 * 
-			 * if (dist < minDist) { minDist = dist; b = j; } } }
-			 */
-
 		}
 	}
 }
