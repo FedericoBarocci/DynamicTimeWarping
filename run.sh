73873aa33a0afa3185f2344af0c91914253e0306
fbarocci <federico.barocci@gmail.com>
2015-07-20 16:26:15 +0200

    add missing files

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 params="data/filter_eventlog.csv 2 24"
 
 apache_commons_path="libs/commons-lang3-3.4.jar"
-guice_path="libs/guice-3.0.jar:libs/aopalliance-1.0.jar:libs/javax.inject.jar:libs/guice-assistedinject-3.0.jar"
+guice_path="libs/guice-4.0.jar:libs/aopalliance-1.0.jar:libs/javax.inject.jar"
 
 java -cp ${guice_path}:${apache_commons_path}:bin logAnalizer.main.Main ${params}
